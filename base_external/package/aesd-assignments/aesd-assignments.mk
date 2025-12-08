3658270296acc1c585a69664e040d5fe565ddd40
Sidd Jove <siddjove@gmail.com>
2025-12-08 13:30:35 +0530

    Add aesd-assignments external package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,40 +1,29 @@
 ################################################################################
-# AESD Assignments Buildroot Package
+#
+# aesd-assignments
+#
 ################################################################################
 
-AESD_ASSIGNMENTS_VERSION = 1
-AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_VERSION = master
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-siddjove.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
 
-AESD_ASSIGNMENTS_INSTALL_STAGING = YES
-AESD_ASSIGNMENTS_LICENSE = MIT
-
-AESD_ASSIGNMENTS_INSTALL_TARGET = YES
+AESD_ASSIGNMENTS_DEPENDENCIES =
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) -C $(@D) CROSS_COMPILE=$(CROSS_COMPILE) CC="$(TARGET_CC)"
+	$(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)"
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(TARGET_DIR)/usr/bin
+	# Scripts and binaries into /usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/tester.sh $(TARGET_DIR)/usr/bin/tester.sh || true
+
+	# Config files into /etc/finder-app/conf
 	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
-
-	if [ -f $(@D)/finder-test.sh ]; then \
-		$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh; \
-	fi
-
-	if [ -f $(@D)/finder.sh ]; then \
-		$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin/finder.sh; \
-	fi
-
-	if [ -f $(@D)/writer ]; then \
-		$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/usr/bin/writer; \
-	fi
-
-	if [ -d $(@D)/conf ]; then \
-		$(CP) -r $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/; \
-	fi
+	cp -r $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 endef
 
 $(eval $(generic-package))
-
